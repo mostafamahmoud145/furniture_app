f1dcaffe6e73c2d9adcd89a617e253e8366d4105
mostafamahmoud145 <mostafamahmoud.mm401@gmail.com>
2025-01-10 18:17:00 +0200

    --

--- lib/route/routes_names.dart
+++ lib/route/routes_names.dart
@@ -3,6 +3,7 @@ class RoutersNames {
   static const String home = '/home';
   static const String products = '/products';
   static const String productDetails = '/productDetails';
+  static const String addProduct = '/addProduct';
   static const String screen1 = '/screen1';
   static const String screen1Section1 = '/section1';
   static const String screen2 = '/screen2';
